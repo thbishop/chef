3c2dd212007d8df55d2c53c5159f0884d2b16887
Tommy Bishop <bishop.thomas@gmail.com>
2010-12-29 00:48:46 -0800

    Clean up knife cookbook download spec

--- chef/spec/unit/knife/cookbook_download_spec.rb
+++ chef/spec/unit/knife/cookbook_download_spec.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Thomas Bishop (<bishop.thomas@gmail.com>)
-# Copyright:: Copyright (c) Thomas Bishop
+# Copyright:: Copyright (c) 2010 Thomas Bishop
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,14 +19,28 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
 
 describe Chef::Knife::CookbookDownload do
-  def set_expectations_for_cookbook_download(cookbook_name, cookbook_version)
-    @rest.should_receive(:get_rest).with("cookbooks/#{cookbook_name}/#{cookbook_version}").and_return(@cookbook_data)
 
-    Chef::Log.should_receive(:info).with("Downloading #{cookbook_name} cookbook version #{cookbook_version}")
-    Chef::Log.should_receive(:info).with("Cookbook downloaded to #{@download_dir}")
+  before(:each) do
+    @knife = Chef::Knife::CookbookDownload.new
+    @stdout = StringIO.new
+    @knife.stub!(:stdout).and_return(@stdout)
+    @rest = mock("Chef::REST")
+
+    @log_stringio = StringIO.new
+    @logger = Logger.new(@log_stringio)
+    @original_chef_logger = Chef::Log.logger
+    Chef::Log.logger = @logger
+  end
+
+  after do
+    Chef::Log.logger = @original_chef_logger
+  end
+
+  def set_expectations_for_cookbook_download
+    @rest.should_receive(:get_rest).with("cookbooks/#{@cookbook_name}/#{@cookbook_version}").and_return(@cookbook_data)
 
     Chef::CookbookVersion::COOKBOOK_SEGMENTS.each do |segment|
-      Chef::Log.should_receive(:info).with("Downloading #{segment}")
+      @chef_log_info_messages << "Downloading #{segment}"
 
       case segment
       when :files, :templates
@@ -38,22 +52,15 @@ describe Chef::Knife::CookbookDownload do
       end
 
       @cookbook_data.manifest[segment].each do |segment_file|
-        segment_file['url'] = "cookbooks/#{cookbook_name}/#{cookbook_version}/#{segment_file['name']}"
+        segment_file['url'] = "cookbooks/#{@cookbook_name}/#{@cookbook_version}/#{segment_file['name']}"
         @temp_file_mock = mock()
         @temp_file_mock.should_receive(:path).and_return("/var/tmp/#{segment_file['name']}")
         FileUtils.should_receive(:mv).with("/var/tmp/#{segment_file['name']}", "#{@download_dir}/#{segment_file['path']}")
         @rest.should_receive(:sign_on_redirect=).with(false)
-        @rest.should_receive(:get_rest).with("cookbooks/#{cookbook_name}/#{cookbook_version}/#{segment_file['name']}", true).and_return(@temp_file_mock)
+        @rest.should_receive(:get_rest).with("cookbooks/#{@cookbook_name}/#{@cookbook_version}/#{segment_file['name']}", true).and_return(@temp_file_mock)
       end
 
     end
-  end
-
-  before(:each) do
-    @knife = Chef::Knife::CookbookDownload.new
-    @stdout = StringIO.new
-    @knife.stub!(:stdout).and_return(@stdout)
-    @rest = mock("Chef::REST")
   end
 
   describe "run" do
@@ -88,6 +95,7 @@ describe Chef::Knife::CookbookDownload do
       before(:each) do
         @cookbook_version = '0.1.0'
         @knife.name_args = [@cookbook_name]
+        @chef_log_info_messages = []
       end
 
       describe "and there is only one version of the cookbook" do
@@ -102,8 +110,14 @@ describe Chef::Knife::CookbookDownload do
           FileUtils.should_receive(:mkdir_p).with(@download_dir)
           File.should_receive(:exists?).with(@download_dir).and_return(false)
           FileUtils.should_not_receive(:rm_rf).with(@download_dir)
-          set_expectations_for_cookbook_download(@cookbook_name, @cookbook_version)
+          set_expectations_for_cookbook_download
           @knife.run
+          @log_stringio.string.should match(Regexp.escape("INFO -- : Downloading #{@cookbook_name} cookbook version #{@cookbook_version}"))
+          @log_stringio.string.should match(Regexp.escape("INFO -- : Cookbook downloaded to #{@download_dir}"))
+          @chef_log_info_messages.count.should > 0
+          @chef_log_info_messages.each do |message|
+            @log_stringio.string.should match(Regexp.escape("INFO -- : #{message}"))
+          end
         end
 
         it "should download the cookbook into the directory we specified with -d or --dir" do
@@ -112,19 +126,23 @@ describe Chef::Knife::CookbookDownload do
           File.should_receive(:exists?).with(@download_dir).and_return(false)
           FileUtils.should_not_receive(:rm_rf).with(@download_dir)
           FileUtils.should_receive(:mkdir_p).with(@download_dir)
-          set_expectations_for_cookbook_download(@cookbook_name, @cookbook_version)
+          set_expectations_for_cookbook_download
           @knife.run
+          @chef_log_info_messages.count.should > 0
+          @chef_log_info_messages.each do |message|
+            @log_stringio.string.should match(Regexp.escape("INFO -- : #{message}"))
+          end
         end
 
         describe "and the download directory already exists" do
           it "should not download but should log an error and exit if we haven't specified to force the download" do
             lambda {
               @rest.should_receive(:get_rest).with("cookbooks/#{@cookbook_name}/#{@cookbook_version}").and_return(@cookbook_data)
-              Chef::Log.should_receive(:info).with("Downloading #{@cookbook_name} cookbook version #{@cookbook_version}")
-              Chef::Log.should_receive(:fatal).with("Directory #{@download_dir} exists, use --force to overwrite")
               File.should_receive(:exists?).with(@download_dir).and_return(true)
               @knife.run
             }.should raise_error(SystemExit) { |e| e.status.should == 0 }
+            @log_stringio.string.should match(Regexp.escape("INFO -- : Downloading #{@cookbook_name} cookbook version #{@cookbook_version}"))
+            @log_stringio.string.should match(Regexp.escape("FATAL -- : Directory #{@download_dir} exists, use --force to overwrite"))
           end
 
           it "should remove the existing directory and download the cookbook when we specify -f or --force" do
@@ -132,8 +150,12 @@ describe Chef::Knife::CookbookDownload do
             FileUtils.should_receive(:mkdir_p).with(@download_dir)
             File.should_receive(:exists?).with(@download_dir).and_return(true)
             FileUtils.should_receive(:rm_rf).with(@download_dir).and_return(true)
-            set_expectations_for_cookbook_download(@cookbook_name, @cookbook_version)
+            set_expectations_for_cookbook_download
             @knife.run
+            @chef_log_info_messages.count.should > 0
+            @chef_log_info_messages.each do |message|
+              @log_stringio.string.should match(Regexp.escape("INFO -- : #{message}"))
+            end
           end
         end
 
@@ -151,28 +173,36 @@ describe Chef::Knife::CookbookDownload do
           @knife.config[:latest] = true
           FileUtils.should_receive(:mkdir_p).with(@download_dir)
           File.should_receive(:exists?).with(@download_dir).and_return(false)
-          set_expectations_for_cookbook_download(@cookbook_name, @cookbook_version)
+          set_expectations_for_cookbook_download
           @knife.stub!(:rest).and_return(@rest)
           @knife.run
+          @chef_log_info_messages.count.should > 0
+          @chef_log_info_messages.each do |message|
+            @log_stringio.string.should match(Regexp.escape("INFO -- : #{message}"))
+          end
         end
 
         it "should prompt us for a selection and log an error and exit when we specify an invalid option" do
           lambda {
-          STDIN.should_receive(:readline).and_return("10\n")
-          Chef::Log.should_receive(:error).with("'10' is not a valid value.")
+          STDIN.stub!(:readline).and_return("10\n")
           @knife.run
           @stdout.string.should match(/Which version do you want to download\?\n1\. #{@cookbook_name} 0\.1\.0\n2\. #{@cookbook_name} 0\.2\.0\n\n/)
           }.should raise_error(SystemExit) { |e| e.status.should == 1 }
+          @log_stringio.string.should match(Regexp.escape("'10' is not a valid value."))
         end
 
         it "should prompt us for a selection and download the version we select" do
-          STDIN.should_receive(:readline).and_return("2\n")
+          STDIN.stub!(:readline).and_return("2\n")
           FileUtils.should_receive(:mkdir_p).with(@download_dir)
           File.should_receive(:exists?).with(@download_dir).and_return(false)
-          set_expectations_for_cookbook_download(@cookbook_name, @cookbook_version)
+          set_expectations_for_cookbook_download
           @knife.stub!(:rest).and_return(@rest)
           @knife.run
           @stdout.string.should match(/Which version do you want to download\?\n1\. #{@cookbook_name} 0\.1\.0\n2\. #{@cookbook_name} 0\.2\.0\n\n/)
+          @chef_log_info_messages.count.should > 0
+          @chef_log_info_messages.each do |message|
+            @log_stringio.string.should match(Regexp.escape("INFO -- : #{message}"))
+          end
         end
       end
 
@@ -183,9 +213,9 @@ describe Chef::Knife::CookbookDownload do
         @knife.name_args = []
         lambda {
           @knife.should_receive(:show_usage)
-          Chef::Log.should_receive(:fatal).with('You must specify a cookbook name')
           @knife.run
         }.should raise_error(SystemExit) { |e| e.status.should == 1}
+        @log_stringio.string.should match(Regexp.escape("You must specify a cookbook name"))
       end
     end
 
